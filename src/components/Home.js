4ff2b14989eb4c960bcfb194156f7ba1dda300ca
hylobates-lar <alison.v.quaglia@gmail.com>
2020-04-14 11:12:37 -0400

    saved carousel images to public folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,35 +12,35 @@ export default function Home() {
             <div className="carousel-container">
                 <Carousel showArrows={true} autoPlay={true} showThumbs={false} infiniteLoop={true}>
                     <div>
-                        <img src="https://images.prismic.io/rvshare/936b26fe018a6a3aae817aed4e4329bfd5e5d7da_zion-8.jpg" alt="Zion National Park"/>
+                        <img src="/zion.jpg" alt="Zion National Park"/>
                         <p className="legend">Zion National Park</p>
                     </div>
                     <div>
-                        <img src="https://www.nationalparks.org/sites/default/files/styles/wysiwyg_full_1x/public/iStock_47779594_MEDIUM.jpg" alt="Grand Canyon National Park"/>
+                        <img src="/grand-canyon.jpg" alt="Grand Canyon National Park"/>
                         <p className="legend">Grand Canyon National Park</p>
                     </div>
                     <div>
-                        <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/valley-view-yosemite-national-park-scott-mcguire.jpg" alt="Yosemite National Park"/>
+                        <img src="/yosemite.jpg" alt="Yosemite National Park"/>
                         <p className="legend">Yosemite National Park</p>
                     </div>
                     <div>
-                        <img src="https://www.wildmoments.net/images/large/brycedone1.jpg" alt="Bryce Canyon National Park"/>
+                        <img src="/bryce-canyon.jpg" alt="Bryce Canyon National Park"/>
                         <p className="legend">Bryce Canyon National Park</p>
                     </div>
                     <div>
-                        <img src="http://www.destinationparks.com/images/park/saguaro-national-park-1280x853.jpg" alt="Saguaro National Park"/>
+                        <img src="/saguaro.jpg" alt="Saguaro National Park"/>
                         <p className="legend">Saguaro National Park</p>
                     </div>
                     <div>
-                        <img src="https://www.scenic-safaris.com/files/large/950b806f511401b" alt="Yellowstone National Park"/>
+                        <img src="/yellowstone.jpg" alt="Yellowstone National Park"/>
                         <p className="legend">Yellowstone National Park</p>
                     </div>
                     <div>
-                        <img src="https://www.davesoldanoimages.com/img/s/v-10/p244139833-3.jpg" alt="Canyonlands National Park"/>
+                        <img src="/canyonlands.jpg" alt="Canyonlands National Park"/>
                         <p className="legend">Canyonlands National Park</p>
                     </div>
                     <div>
-                        <img src="https://www.chaletvillage.com/wp-content/uploads/2017/05/Sunrise-in-the-mountains-near-Gatlinburg.jpg" alt="Great Smoky Mountains National Park"/>
+                        <img src="/great-smoky-mountains.jpg" alt="Great Smoky Mountains National Park"/>
                         <p className="legend">Great Smoky Mountains National Park</p>
                     </div>
                 </Carousel>
